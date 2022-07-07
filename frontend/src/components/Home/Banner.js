4527bf389bd991571087bd76f81c9d643482ff3e
Andrei Averkin <andrei@Andreis-MacBook-Pro.local>
2022-07-07 11:39:32 +0300

    format

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -16,16 +16,18 @@ const Banner = (props) => {
   };
 
   const openSearch = () => {
-    const element = document.getElementById('search-box');
+    const element = document.getElementById("search-box");
     element.classList.remove("d-none");
-  };  
+  };
 
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part" onClick={openSearch}>A place to get</span>
+          <span id="get-part" onClick={openSearch}>
+            A place to get
+          </span>
           <input
             id="search-box"
             className="d-none rounded-pill m-2 pt-2 pb-2 pl-3 pr-3 w-50"
