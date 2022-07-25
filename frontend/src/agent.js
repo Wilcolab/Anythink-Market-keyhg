b622dcb95b3b093ae239d7703ff33a21383526ce
Andrei Averkin <andrei@Andreis-MacBook-Pro.local>
2022-07-25 16:10:39 +0300

    re-stam

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -8,7 +8,6 @@ const API_ROOT =
     ? "http://localhost:3000/api"
     : "https://anythink-market-keyhg-api.herokuapp.com";
 
-
 const encode = encodeURIComponent;
 const responseBody = (res) => res.body;
 
