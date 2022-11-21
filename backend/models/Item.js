e24c3b8f5852932dc60dc57ba975462ab12ce3d3
Andrei <andrei.averkin@gmail.com>
2022-11-21 17:39:23 +0200

    Update Item.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -49,7 +49,7 @@ ItemSchema.methods.toJSONFor = function(user) {
     slug: this.slug,
     title: this.title,
     description: this.description,
-    image: this.image || 'sk-',
+    image: this.image || 'sk-https://oaidalleapiprodscus.blob.core.windows.net/private/org-rXr9TScdKgJKnW0988kfsI4b/user-SJeynRVHFZT4KbZdSPJU97ab/img-diMioWWqivgD8fcRbsw9dILX.png?st=2022-11-21T10%3A38%3A01Z&se=2022-11-21T12%3A38%3A01Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-11-21T08%3A06%3A11Z&ske=2022-11-22T08%3A06%3A11Z&sks=b&skv=2021-08-06&sig=QP2TUXRMEkWhr9692W73II5lROmFQHJ6N9peldT52Xo%3D',
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
