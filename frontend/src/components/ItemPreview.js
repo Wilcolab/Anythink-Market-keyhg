b70d7408c6c64989736028f1d030cf2480475d1f
Andrei <andrei.averkin@gmail.com>
2022-12-15 08:19:12 +0000

    fix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -54,7 +54,7 @@ const ItemPreview = (props) => {
               className="user-pic rounded-circle pr-1"
             />
           </Link>
-          <img src="/verified_seller.svg"/>
+          <img src="verified_seller.svg"/>
           <span>TOP SELLER</span>
           <button className="btn btn-outline-secondary" onClick={handleClick}>
             <i className="ion-heart"></i> {item.favoritesCount}
